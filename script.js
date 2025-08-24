ada09299e383d6fd814ede0a6f23eba237daadca
Lukman Hammed <tec.lukman@gmail.com>
2025-08-24 21:56:37 +0100

    created the add note function

--- script.js
+++ script.js
@@ -1,4 +1,33 @@
 const btnEl = document.getElimentById("btn");
-const appsEl = documant.getElimentById("apps");
+const appEl = document.getElimentById("apps");
 
+let notes = [];
+
+function createNoteEl(id, content){
+
+  const element = document.createElement("textarea");
+  element.classList.add("note");
+  element.placeholder = "Empty Note";
+  element.value = content;
+
+  return element;
+}
+
+
+
+function addNote(){
+  //  id, content
+
+  const noteObj = {
+    id: Math.floor(Math.random() * 10000),
+    content: ""
+  };
+
+  const noteEl = createNoteEl(noteObj.id, noteObj.content);
+  appEl.insertBefore(noteEl, btnEl);
+
+  notes.push(noteObj);
+}
+
+btnEl.addEventListeners("click", addNote);
 
